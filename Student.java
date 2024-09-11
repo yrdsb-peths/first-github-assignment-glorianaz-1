800ff667280f57b224f229e36a4bdbcb628ff125
glorianaz <150281309+glorianaz@users.noreply.github.com>
2024-09-11 12:58:28 -0400

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student()
+{
+  public String firstName();
+  public String lastName();
+  public int gradeLevel();
+
+  public Student(String fName, String lName, int grade)
+  {
+    firstName = fName;
+    lastName = lName;
+    gradeLevel = grade; 
+  }
+}
